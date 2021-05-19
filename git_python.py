16d52babdb5af32988259bfc6ae757370e43e155
Osmin Larreynaga <hectorosminn@gmail.com>
2021-05-19 17:07:05 -0600

    Segundo commit, creo que estoy comenzando a ver la luz jijijij

--- git_python.py
+++ git_python.py
@@ -1,5 +1,12 @@
 
 print("holis mundo que tal, probando git, btw")
 
+#               Recien creado el archivo
 #como no se que pedo en el video pues estoy aqui valiendo madres jijiji 
 # esperen por más
+
+#               Primer Commit
+#Bueno probe el comando git restores y borro lo que habia hecho JAJAJAJA XDa 
+#Ya estoy entendiendo esta webada jijijijij
+
+#               Segundo Commit
