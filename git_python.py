321932f1b9701a9ba2f880c24cdec4428d72da58
Osmin Larreynaga <hectorosminn@gmail.com>
2021-05-19 17:25:53 -0600

    Cuarto commint mande todo a la kk

--- git_python.py
+++ git_python.py
@@ -1,20 +1,29 @@
 
 print("holis mundo que tal, probando git, btw")
 
-#               Recien creado el archivo
-#como no se que pedo en el video pues estoy aqui valiendo madres jijiji 
-# esperen por más
+#           Quizas 4 commit
+#He mandado a la kk todo, y pues lo voy a incertar de otra manera, hold
 
-#               Primer Commit
-#Bueno probe el comando git restores y borro lo que habia hecho JAJAJAJA XDa 
-#Ya estoy entendiendo esta webada jijijijij
+print(" Que pedo mis doggies? Yo ando aqui aprendiendo git & github JAJAJA :c")
 
-#               Segundo Commit
-#Hoy si ya esta el segundo commit, seguire viendo el video tutorial XD
-#Mantengase al tanto, cambio y fuera.
+''' 
+              Recien creado el archivo
+como no se que pedo en el video pues estoy aqui valiendo madres jijiji 
+esperen por más
+
+              Primer Commit
+Bueno probe el comando git restores y borro lo que habia hecho JAJAJAJA XDa 
+Ya estoy entendiendo esta webada jijijijij
+
+              Segundo Commit
+Hoy si ya esta el segundo commit, seguire viendo el video tutorial XD
+Mantengase al tanto, cambio y fuera.
 
 
-#               Tercer commit
-#Pos crei que ya iba entendiendo, ahora me siento confundido weyyyy 
-#Seguiremos viendo que pedo, cambio y fuera.
+              Tercer commit
+Pos crei que ya iba entendiendo, ahora me siento confundido weyyyy 
+Seguiremos viendo que pedo, cambio y fuera.
+'''
 
+#Acabo de revisar Soruce control de Code y pues he visto los cambios con las lineas quitas y agregadas
+#Voy hacer un commit y antes un add a ver que pex, luego lo haré al reves escribiendo más cosas randoms jijij
